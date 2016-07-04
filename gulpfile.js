a21f634752d3cb8029209694ee1c261cec778a8b
colmer <colmer@yandex.ru>
2016-07-04 19:20:29 +0400

    Add pug and browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	sass = require('gulp-sass'),
 	runSequence = require('run-sequence'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	pug = require('gulp-pug'),
+	browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
-	gulp.src([
+	return gulp.src([
         './dev/sass/index.sass'
 	])
 	.pipe(sass().on('error', sass.logError))
@@ -13,10 +15,28 @@ gulp.task('sass', function(){
 	.pipe(gulp.dest('./public'));
 });
 
+gulp.task('pug', function(){
+	return gulp.src([
+        './dev/pug/*.pug'
+	])
+	.pipe(pug())
+	.pipe(gulp.dest('./public'));
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'pug'], function() {
+
+    browserSync.init({
+        server: "./public"
+    });
+
+    gulp.watch("./dev/sass/*.sass", ['sass']);
+    gulp.watch("./dev/pug/*.pug", ['pug']);
+    gulp.watch("public/*.html").on('change', browserSync.reload);
+});
+
 gulp.task('default', function(callback){
 	gutil.log(gutil.colors.green('Start creating package!'));
 
-	runSequence(['sass'], callback);
-
-	gutil.log(gutil.colors.green('Create package is done!'));
+	runSequence(['sass', 'jade'], callback);
 });
